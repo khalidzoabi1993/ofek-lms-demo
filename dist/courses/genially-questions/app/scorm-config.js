90e42ca89eb0a9a7dacc74b37d4f4f59d5ba8164
Khalid Zoabi <khalidzoabi1993@gmail.com>
2026-09-25 08:09:29 +0300

    TES-61: integrate official Genially quiz upload and SCORM reporting

---
+++ dist/courses/genially-questions/app/scorm-config.js
@@ -0,0 +1,6 @@
+
+    window.geniallyElearningQuestionsPresetData = {
+      passingPercentage: '80',
+      scoringAnswerIds: '{"ebddd8af-bb1d-4739-aeb7-5ba3b6dbdd04":10,"ac369eee-fde1-441e-8704-efa919265e88":10,"220d8810-f863-4fff-803c-670162f25e98":10,"249f5fe0-13c2-4e92-84f8-f56f49dd8f19":10,"13cc7602-7228-4c5b-97c6-74d0676dee83":10,"cc1016aa-9bd6-4ccc-ad01-0a030c305a3b":10,"901d94ae-2d9f-4541-99f9-2f4a649984df":10,"242b638b-ef73-4abe-8d89-5ad4e770791f":10,"f58db34e-c00b-4a46-86c3-f8dcbb027990":10,"91c38f90-a579-45db-b3d1-ddcf60e2d8f7":10}'
+    };
+    
